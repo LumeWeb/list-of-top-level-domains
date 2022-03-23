5b9686ecdfebc4f46adbca3b795e759386ddc82d
Derrick Hammer <derrick@derrickhammer.com>
2022-03-23 01:28:49 -0400

    *Add typings

---
+++ formats/js/tld-enum/index.d.ts
@@ -0,0 +1,22 @@
+export interface Desc {
+    [domain: string]: string;
+}
+
+export interface InfoItem {
+    domain: string;
+    description: string;
+    type: string;
+}
+
+export interface Type {
+    [domain: string]: string;
+}
+
+export interface tldEnum {
+    desc: Desc;
+    info: InfoItem[];
+    list: string[];
+    type: Type;
+}
+
+export default tldEnum;
